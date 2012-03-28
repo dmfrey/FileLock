3b21fe50a186eca3e9b40cf951ab95ad2d45790b
Daniel Frey <dfrey@knoll.com>
2012-03-27 20:55:04 -0400

    added original author

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 setup(
     name='FileLock',
     version='0.1.0',
-    author='dmfrey',
-    author_email='dmfrey@gmail.com',
+    author='Evan Fosmark',
+    author_email='me@evanfosmark.com',
     packages=['filelock','filelock.test'],
-    url='http://test.com',
+    url='https://github.com/dmfrey/FileLock',
     license='LICENSE.txt',
     description='File locking library',
     long_description=open('README.txt').read()
